492cde70336f46bd391f7473b31a9b0ab794a84c
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2023-03-02 10:38:49 +0500

    Added TODOs

--- lib/widgets/menu_page.dart
+++ lib/widgets/menu_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:menu_application/widgets/menu_widget.dart';
 
+// TODO: вынести в папку pages
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
 
--- lib/widgets/menu_widget.dart
+++ lib/widgets/menu_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:menu_application/data.dart';
 import 'menu_widget_row.dart';
 
+// TODO: Один файл - один класс! Можешь вынести в файл text_and_icon_model.dart
 class TextAndIcon {
   TextAndIcon({required this.text, required this.icon});
 
@@ -14,6 +15,16 @@ class MenuWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /* 
+    TODO: нет смысла выносить это в отдельные переменные, особенно 
+    в функции build, которая вызывается при каждой перерисовке. 
+    Раз эти переменные вызываются один раз, легче напрямую передавать данные 
+    (icon:  Data.data.values.toList()[index]). А если несколько вызовов, 
+    то лучше за пределами build объявлять переменную или геттер. 
+
+    Вообще в функции build должен быть только return, все нужные вычисления 
+    лучше проводить за её пределами. В данном случае в них даже нет смысла.
+    */
     final List<IconData> icons = Data.data.values.toList();
     final List<String> texts = Data.data.keys.toList();
 
